453024c3a7eba8cb0911d4f8ed5d77f52421913e
dvfens <aryadivyansh01122006@gmail.com>
2025-12-26 06:52:27 +0530

    Add Round Robin CPU scheduling algorithm

--- scheduler.py
+++ scheduler.py
@@ -3,6 +3,7 @@ class Process:
         self.name = name
         self.arrival = arrival
         self.service = service
+        self.remaining= service
 
 
 def fcfs_schedule(processes):
@@ -16,11 +17,57 @@ def fcfs_schedule(processes):
         print(f"Process {process.name} runs from {current_time} to {current_time + process.service}")
         current_time += process.service
 
+from collections import deque
+
+def round_robin_schedule(processes, quantum):
+    processes.sort(key=lambda x: x.arrival)
+    queue = deque()
+    time = 0
+    index = 0
+    n = len(processes)
+
+    print(f"\nRound Robin Scheduling (Quantum = {quantum})")
+
+    while queue or index < n:
+
+        while index < n and processes[index].arrival <= time:
+            queue.append(processes[index])
+            index += 1
+
+        if not queue:
+            time = processes[index].arrival
+            continue
+
+        process = queue.popleft()
+        exec_time = min(quantum, process.remaining)
+
+        print(f"Process {process.name} runs from {time} to {time + exec_time}")
+
+        time += exec_time
+        process.remaining -= exec_time
+
+       
+        while index < n and processes[index].arrival <= time:
+            queue.append(processes[index])
+            index += 1
+
+        if process.remaining > 0:
+            queue.append(process)
+
 if __name__ == "__main__":
-     processes = [
-         Process("P1", arrival=0, service=3),
-         Process("P2", arrival=1, service=2)
-     ]
-     
-     # Run the FCFS scheduler
-     fcfs_schedule(processes)
+    processes = [
+        Process("P1", 0, 5),
+        Process("P2", 1, 3),
+        Process("P3", 2, 1)
+    ]
+
+    fcfs_schedule(processes)
+
+    
+    processes_rr = [
+        Process("P1", 0, 5),
+        Process("P2", 1, 3),
+        Process("P3", 2, 1)
+    ]
+
+    round_robin_schedule(processes_rr, quantum=2)
