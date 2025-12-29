104b501235269b6a0efec85fb8ff9bab6609eaa2
dvfens <aryadivyansh01122006@gmail.com>
2025-12-29 22:42:00 +0530

    Add non-preemptive priority CPU scheduling algorithm

--- core/process.py
+++ core/process.py
@@ -1,9 +1,10 @@
 class Process:
-    def __init__(self, pid, arrival_time, burst_time):
+    def __init__(self, pid, arrival_time, burst_time, priority=0):
         self.pid = pid
         self.arrival_time = arrival_time
         self.burst_time = burst_time
         self.remaining_time = burst_time
+        self.priority = priority
 
         self.waiting_time = 0
         self.turnaround_time = 0
--- main.py
+++ main.py
@@ -1,28 +1,14 @@
 from core.process import Process
-from algorithms.fcfs import fcfs
-from algorithms.round_robin import round_robin
-
-print("CPU Scheduling Simulator Started\n")
+from algorithms.priority import priority_scheduling
 
 processes = [
-    Process(1, 0, 5),
-    Process(2, 1, 3),
-    Process(3, 2, 8)
+    Process(1, 0, 5, priority=2),
+    Process(2, 1, 3, priority=1),
+    Process(3, 2, 8, priority=3),
 ]
 
-print("=== FCFS ===")
-fcfs_result = fcfs(processes)
-for p in fcfs_result:
-    print(f"P{p.pid} WT={p.waiting_time} TAT={p.turnaround_time}")
+print("\n=== Priority Scheduling (Non-Preemptive) ===")
+ps_result = priority_scheduling(processes)
 
-# recreate processes (important)
-processes = [
-    Process(1, 0, 5),
-    Process(2, 1, 3),
-    Process(3, 2, 8)
-]
-
-print("\n=== Round Robin (TQ = 2) ===")
-rr_result = round_robin(processes, time_quantum=2)
-for p in rr_result:
-    print(f"P{p.pid} WT={p.waiting_time} TAT={p.turnaround_time}")
+for p in ps_result:
+    print(f"P{p.pid} PR={p.priority} WT={p.waiting_time} TAT={p.turnaround_time}")
