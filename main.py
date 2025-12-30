78580d38f6226f47083aa0c46972a92141cb561b
dvfens <aryadivyansh01122006@gmail.com>
2025-12-30 22:42:54 +0530

    Add preemptive priority CPU scheduling algorithm

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from core.process import Process
-from algorithms.priority import priority_scheduling
+from algorithms.preemptive_priority import preemptive_priority_scheduling
 
+# recreate processes (important)
 processes = [
     Process(1, 0, 5, priority=2),
     Process(2, 1, 3, priority=1),
     Process(3, 2, 8, priority=3),
 ]
 
-print("\n=== Priority Scheduling (Non-Preemptive) ===")
-ps_result = priority_scheduling(processes)
+print("\n=== Preemptive Priority Scheduling ===")
+pp_result = preemptive_priority_scheduling(processes)
 
-for p in ps_result:
+for p in pp_result:
     print(f"P{p.pid} PR={p.priority} WT={p.waiting_time} TAT={p.turnaround_time}")
