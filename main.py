32101de2cfb584edf63feebde0bdfd64e918a89b
dvfens <aryadivyansh01122006@gmail.com>
2025-12-30 22:52:42 +0530

    Add SJF CPU scheduling algorithms

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from core.process import Process
-from algorithms.preemptive_priority import preemptive_priority_scheduling
+from algorithms.sjf import sjf
 
-# recreate processes (important)
+
+# ---------- SJF ----------
 processes = [
-    Process(1, 0, 5, priority=2),
-    Process(2, 1, 3, priority=1),
-    Process(3, 2, 8, priority=3),
+    Process(1, 0, 5),
+    Process(2, 1, 3),
+    Process(3, 2, 8),
 ]
 
-print("\n=== Preemptive Priority Scheduling ===")
-pp_result = preemptive_priority_scheduling(processes)
+print("\n=== SJF (Non-Preemptive) ===")
+sjf_result = sjf(processes)
+for p in sjf_result:
+    print(f"P{p.pid} WT={p.waiting_time} TAT={p.turnaround_time}")
 
-for p in pp_result:
-    print(f"P{p.pid} PR={p.priority} WT={p.waiting_time} TAT={p.turnaround_time}")
+
+
+
