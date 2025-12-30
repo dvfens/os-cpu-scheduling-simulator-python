32bd9f27d1b6d7101c3b8722bce23d441b8e50ac
dvfens <aryadivyansh01122006@gmail.com>
2025-12-30 22:54:26 +0530

    Add SRTF CPU scheduling algorithms

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from core.process import Process
-from algorithms.sjf import sjf
+from algorithms.srtf import srtf
 
 
-# ---------- SJF ----------
+# ---------- SRTF ----------
 processes = [
     Process(1, 0, 5),
     Process(2, 1, 3),
     Process(3, 2, 8),
 ]
 
-print("\n=== SJF (Non-Preemptive) ===")
-sjf_result = sjf(processes)
-for p in sjf_result:
+print("\n=== SRTF (Preemptive SJF) ===")
+srtf_result = srtf(processes)
+for p in srtf_result:
     print(f"P{p.pid} WT={p.waiting_time} TAT={p.turnaround_time}")
 
 
